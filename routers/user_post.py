01dec3b8ad96a1909f5708ee9b3df48aa2deba3f
ShiragG <gulyayevsgh@mail.ru>
2024-11-24 16:37:55 +0500

    Переименовал файлы

--- routers/user_post.py
+++ routers/user_post.py
@@ -1,11 +1,22 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix='/user',
     tags=['user']
 )
 
+class UserModel(BaseModel):
+    login: str
+    password: str
+    email: str
+    role_id: int
+    first_name: str
+    second_name: str
+    last_name: str | None
+    user_position: str
+
 
 @router.post('/new')
-def create_user():
-    pass
+def create_user(user: UserModel):
+    return 'ok'
